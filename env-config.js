3dc3097d204f82ba3d8140c5b8dd322eee4dcc0d
dwivi <vaishdw2207@gmail.com>
2020-09-24 18:11:32 +0530

    fix: for the last time man :(

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/dwvicy.github.io' : '',
+  'process.env.BACKEND_URL': prod ? '/dwvicy' : '',
 }
