641bf80e795eddac7b7cd27040ec153053c25b0e
dwivi <vaishdw2207@gmail.com>
2020-09-24 18:23:52 +0530

    Deploy Next.js to gh-pages

---
+++ out/_next/static/q6xN7-ajsImoQQkOJihIc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fa4381d7891c03d5396b3.css","static\u002Fchunks\u002Fpages\u002Findex-d9393ef3ca0fa44dfc90.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
