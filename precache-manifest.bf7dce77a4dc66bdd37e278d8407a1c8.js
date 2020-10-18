9c36295ffd4805b144270067f3084f847de62d3b
Vaishnavi <vaishdw2207@gmail.com>
2020-10-18 20:17:00 +0530

    Updates

---
+++ precache-manifest.bf7dce77a4dc66bdd37e278d8407a1c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4204cf14a5ff56088d6eacb9c5cbe2",
+    "url": "/vaishnavidwivedi.me/index.html"
+  },
+  {
+    "revision": "4c02b895096ae4767fab",
+    "url": "/vaishnavidwivedi.me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dace23ab03bb86cf0198",
+    "url": "/vaishnavidwivedi.me/static/js/2.495e09b5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/vaishnavidwivedi.me/static/js/2.495e09b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c02b895096ae4767fab",
+    "url": "/vaishnavidwivedi.me/static/js/main.225967c3.chunk.js"
+  },
+  {
+    "revision": "5ff8a18f664073d05866",
+    "url": "/vaishnavidwivedi.me/static/js/runtime-main.337b50de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vaishnavidwivedi.me/static/media/logo.5d5d9eef.svg"
+  }
+]);
