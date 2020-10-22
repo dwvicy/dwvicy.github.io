2e3a971b4a8d2484a8501021e8e8c4e6fd2680df
Vaishnavi <vaishdw2207@gmail.com>
2020-10-22 22:25:27 +0530

    fix: remove build from .gitignore

---
+++ build/precache-manifest.f4aaa9de5b0dbe70ff935ba5645203be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2115d75f89ccd3e26300d0876f4468",
+    "url": "./index.html"
+  },
+  {
+    "revision": "137d7790b244024fe3e0",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a2e25cedac661fb395b",
+    "url": "./static/js/2.f9d47a40.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.f9d47a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137d7790b244024fe3e0",
+    "url": "./static/js/main.ab6f0248.chunk.js"
+  },
+  {
+    "revision": "70631afae95ea0acb8b4",
+    "url": "./static/js/runtime-main.ac138d89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
