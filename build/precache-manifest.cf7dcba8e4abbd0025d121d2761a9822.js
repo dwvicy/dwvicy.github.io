c066d0ce70156dbb69beea7e784ea3a420d6fbb7
Vaishnavi <vaishdw2207@gmail.com>
2020-12-04 02:00:23 +0530

    build: changes

--- build/precache-manifest.cf7dcba8e4abbd0025d121d2761a9822.js
+++ build/precache-manifest.cf7dcba8e4abbd0025d121d2761a9822.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a51f2afee600f53c423acc1da5395b63",
+    "revision": "4cefb4924073520a7a9896861d074c29",
     "url": "./index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.c31cb8f6.chunk.css"
   },
   {
-    "revision": "6d8c9d66e9effbfd9c0a",
-    "url": "./static/css/main.0aec76f2.chunk.css"
+    "revision": "1dbc785bd4384415a2dc",
+    "url": "./static/css/main.b05e7089.chunk.css"
   },
   {
     "revision": "2250ab63dbdbde1b0e36",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.937c59b3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6d8c9d66e9effbfd9c0a",
-    "url": "./static/js/main.58f6fce6.chunk.js"
+    "revision": "1dbc785bd4384415a2dc",
+    "url": "./static/js/main.ad58b5d1.chunk.js"
   },
   {
     "revision": "70631afae95ea0acb8b4",
