bc4266865dcbde60df0954ceb5bd5e6373381086
Vaishnavi <vaishdw2207@gmail.com>
2020-12-27 02:02:17 +0530

    fix: minor changes in alignment

--- src/components/TypeWrittingEffect.js
+++ src/components/TypeWrittingEffect.js
@@ -17,17 +17,17 @@ options={
 className="big-font"
   onInit={(typewriter) => {
     typewriter.changeDelay(55)
-    .typeString('<span style="font-size: larger">Hello, I\'m <b> Vaishnavi</b>! :)</span></br></br>')
+    .typeString('<span style="font-size: larger">Hello, I\'m <b> Vaishnavi</b>! :)</span>  ')
     .pauseFor(900)
     .typeString('<span style="font-size: larger">Who am I?</span>')
     .pauseFor(1000)
     .deleteChars(9)
     
-    .typeString('<span style="font-size: larger">I\'m a <b>Software Developer</b> and <b>Web </br> Scrum Master</b> @ <a href="https://www.bottlehq.com/"><span style="color: #56A286"><b>Bottle Co.</b></span></a></span></br><br>')
-    .typeString('<span style="font-size: larger">On August 12, 2020, I was appointed</br> <strong>Developer Student Club Lead</strong> at my</br> campus')
-    .typeString('<span style="font-size: larger"> by <span style="color: #f4c20d"><b>Google</b>.</span></br> Since then I\'ve hosted <b>20+  technical </br>talks</b> and sessions for over</br> <b>500 members</b> of our club. </span></br><br>')
-    .typeString('<span style="font-size: larger"> I\'m a staunch supporter of<b> climate </br>action </b>and<b> sustainability</b>.</br> I\'m passionate about solving </br><b>global real-world challenges</b> using</br> <b>Data</b>, while preserving the <b>privacy</b></br> of the owners. I\'ve written many <i>blogs</i></br> and actively <b>researching</b> in these</br> areas. </span></br><br>')
-    .typeString('<span style="font-size: larger">Get to know me better by navigating</br> through this portfolio!</span>')
+    .typeString('<span style="font-size: larger">I\'m a <b>Software Developer</b> and <b>Web Scrum Master</b> @ <a href="https://www.bottlehq.com/"><span style="color: #56A286"><b>Bottle Co.</b></span></a></span>  <br><br>')
+    .typeString('<span style="font-size: larger">On August 12, 2020, I was appointed <strong>Developer Student Club Lead</strong> at my campus')
+    .typeString('<span style="font-size: larger"> by <span style="color: #f4c20d"><b>Google</b>.</span>  Since then I\'ve hosted <b>20+ technical  talks</b> and sessions for over <b>500 members</b> of our club. </span>  <br><br>')
+    .typeString('<span style="font-size: larger"> I\'m a staunch supporter of<b> climate  action </b>and<b> sustainability</b>. I\'m passionate about solving  <b>global real-world challenges</b> using <b>Data</b>, while preserving the <b>privacy</b> of the owners. I\'ve written many <i>blogs</i>  and actively <b>researching</b> in these areas.</span>  <br><br>')
+    .typeString('<span style="font-size: larger">Get to know me better by navigating  through this portfolio!</span>')
     // .pauseFor(2500)
     // .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
     .start();
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import TypeEff from '../components/TypeWrittingEffect'
  
 function About(props) {
   return (
-    <div class="my-10 mx-10">
+    <div class="my-20 mx-20">
       <h1 style={{fontSize:"x-large"}}>A little bit about me</h1>
       <TypeEff/>
     </div>
